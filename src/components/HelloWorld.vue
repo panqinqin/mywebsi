53a366a484785edb3edd9dfa862ce174b4f6fd16
panqingqing <panqingqing@reconova.com>
2020-04-16 10:54:07 +0800

    首次提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,260 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div style="width:100%;height:50%">
+        <canvas style="width:100%;height:100%" id="canvas"></canvas>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    import gsap from "gsap";
+
+    export default {
+        name: "InformationRegis",
+        data() {
+            return {}
+        },
+        mounted() {
+            console.log(gsap)
+            const config = {
+                src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/175711/open-peeps-sheet.png',
+                rows: 15,
+                cols: 7
+            }
+
+            // UTILS
+
+            const randomRange = (min, max) => min + Math.random() * (max - min)
+
+            const randomIndex = (array) => randomRange(0, array.length) | 0
+
+            const removeFromArray = (array, i) => array.splice(i, 1)[0]
+
+            const removeItemFromArray = (array, item) => removeFromArray(array, array.indexOf(item))
+
+            const removeRandomFromArray = (array) => removeFromArray(array, randomIndex(array))
+
+            const getRandomFromArray = (array) => (
+                array[randomIndex(array) | 0]
+            )
+
+            // TWEEN FACTORIES
+
+            const resetPeep = ({stage, peep}) => {
+                const direction = Math.random() > 0.5 ? 1 : -1
+                // using an ease function to skew random to lower values to help hide that peeps have no legs
+                const offsetY = 100 - 250 * gsap.parseEase('power2.in')(Math.random())
+                const startY = stage.height - peep.height + offsetY
+                let startX
+                let endX
+
+                if (direction === 1) {
+                    startX = -peep.width
+                    endX = stage.width
+                    peep.scaleX = 1
+                } else {
+                    startX = stage.width + peep.width
+                    endX = 0
+                    peep.scaleX = -1
+                }
+
+                peep.x = startX
+                peep.y = startY
+                peep.anchorY = startY
+
+                return {
+                    startX,
+                    startY,
+                    endX
+                }
+            }
+
+            const normalWalk = ({peep, props}) => {
+                const {
+                    startY,
+                    endX
+                } = props
+
+                const xDuration = 10
+                const yDuration = 0.25
+
+                const tl = gsap.timeline()
+                tl.timeScale(randomRange(0.5, 1.5))
+                tl.to(peep, {
+                    duration: xDuration,
+                    x: endX,
+                    ease: 'none'
+                }, 0)
+                tl.to(peep, {
+                    duration: yDuration,
+                    repeat: xDuration / yDuration,
+                    yoyo: true,
+                    y: startY - 10
+                }, 0)
+
+                return tl
+            }
+
+            const walks = [
+                normalWalk,
+            ]
+
+            // CLASSES
+
+            class Peep {
+                constructor({
+                                image,
+                                rect,
+                            }) {
+                    this.image = image
+                    this.setRect(rect)
+
+                    this.x = 0
+                    this.y = 0
+                    this.anchorY = 0
+                    this.scaleX = 1
+                    this.walk = null
+                }
+
+                setRect(rect) {
+                    this.rect = rect
+                    this.width = rect[2]
+                    this.height = rect[3]
+
+                    this.drawArgs = [
+                        this.image,
+                        ...rect,
+                        0, 0, this.width, this.height
+                    ]
+                }
+
+                render(ctx) {
+                    ctx.save()
+                    ctx.translate(this.x, this.y)
+                    ctx.scale(this.scaleX, 1)
+                    ctx.drawImage(...this.drawArgs)
+                    ctx.restore()
+                }
+            }
+
+            // MAIN
+
+            const img = document.createElement('img')
+            img.onload = init
+            img.src = config.src
+
+            const canvas = document.querySelector('#canvas')
+            const ctx = canvas.getContext('2d')
+
+            const stage = {
+                width: 0,
+                height: 0,
+            }
+
+            const allPeeps = []
+            const availablePeeps = []
+            const crowd = []
+
+            function init() {
+                createPeeps()
+
+                // resize also (re)populates the stage
+                resize()
+
+                gsap.ticker.add(render)
+                window.addEventListener('resize', resize)
+            }
+
+            function createPeeps() {
+                const {
+                    rows,
+                    cols
+                } = config
+                const {
+                    naturalWidth: width,
+                    naturalHeight: height
+                } = img
+                const total = rows * cols
+                const rectWidth = width / rows
+                const rectHeight = height / cols
+
+                for (let i = 0; i < total; i++) {
+                    allPeeps.push(new Peep({
+                        image: img,
+                        rect: [
+                            (i % rows) * rectWidth,
+                            (i / rows | 0) * rectHeight,
+                            rectWidth,
+                            rectHeight,
+                        ]
+                    }))
+                }
+            }
+
+            function resize() {
+                stage.width = canvas.clientWidth
+                stage.height = canvas.clientHeight
+                canvas.width = stage.width * devicePixelRatio
+                canvas.height = stage.height * devicePixelRatio
+
+                crowd.forEach((peep) => {
+                    peep.walk.kill()
+                })
+
+                crowd.length = 0
+                availablePeeps.length = 0
+                availablePeeps.push(...allPeeps)
+
+                initCrowd()
+            }
+
+            function initCrowd() {
+                while (availablePeeps.length) {
+                    // setting random tween progress spreads the peeps out
+                    addPeepToCrowd().walk.progress(Math.random())
+                }
+            }
+
+            function addPeepToCrowd() {
+                const peep = removeRandomFromArray(availablePeeps)
+                const walk = getRandomFromArray(walks)({
+                    peep,
+                    props: resetPeep({
+                        peep,
+                        stage,
+                    })
+                }).eventCallback('onComplete', () => {
+                    removePeepFromCrowd(peep)
+                    addPeepToCrowd()
+                })
+
+                peep.walk = walk
+
+                crowd.push(peep)
+                crowd.sort((a, b) => a.anchorY - b.anchorY)
+
+                return peep
+            }
+
+            function removePeepFromCrowd(peep) {
+                removeItemFromArray(crowd, peep)
+                availablePeeps.push(peep)
+            }
+
+            function render() {
+                canvas.width = canvas.width + ''
+                ctx.save()
+                ctx.scale(devicePixelRatio, devicePixelRatio)
+
+                crowd.forEach((peep) => {
+                    peep.render(ctx)
+                })
+
+                ctx.restore()
+            }
+        },
+        methods: {}
+    }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
