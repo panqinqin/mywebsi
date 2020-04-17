cf07814775a483bbce6d4163e30315f3fe11f669
panqingqing <panqingqing@reconova.com>
2020-04-17 18:47:07 +0800

    首次提交

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import ant from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
+Vue.use(ant);
 Vue.config.productionTip = false
 new Vue({
     router,
